9920ad97daf4f174416b4e8bfcfe5db92eb3970f
Saed Nassar <104236159+SaedNassar28@users.noreply.github.com>
2023-06-20 13:20:56 +0300

    Initializing spring app

---
+++ src/main/java/com/teamofcrazymen/coffeeeshop/CoffeeEShopApplication.java
@@ -0,0 +1,13 @@
+package com.teamofcrazymen.coffeeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeEShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamofcrazymen/coffeeeshop/CoffeeEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamofcrazymen.coffeeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
